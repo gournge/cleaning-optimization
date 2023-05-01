83fd5c1bc929cf824b58ceca9a358c0a0ed28665
gournge <filip.morawiec@gmail.com>
2023-05-01 10:30:42 +0200

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import csv
+import numpy as np 
+
+def main():
+
+    room = np.genfromtxt("premade-rooms/room1.csv", delimiter = ',')
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+
+    main()
+
+
+
+
